0b2c4d51a127b99a966407975fed7ebb863edea7
AdamAmusa <92576185+AdamAmusa@users.noreply.github.com>
2024-11-22 10:06:49 +0000

    Added camera taking functionality

--- travel-companion/src/app/home/home.page.ts
+++ travel-companion/src/app/home/home.page.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CameraService } from '../services/camera.service';
+import { IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private camera:CameraService) {}
+
+  async takePicture() {
+    this.camera.takePicture();
+  }
 }
